2eeccdbf68310243e4d80cfdb212efc95ef51106
gitwbr <704231836@qq.com>
2025-02-07 15:42:21 +0000

    更新配置：修改 longpolling 为 websocket

--- create_client.sh
+++ create_client.sh
@@ -152,15 +152,21 @@ server {
     }
     
     # 长轮询配置
-    location /longpolling {
-        proxy_pass http://127.0.0.1:${LONGPOLLING_PORT};
+    location /websocket {
+        proxy_pass http://127.0.0.1:\${LONGPOLLING_PORT};
         proxy_http_version 1.1;
         proxy_set_header Upgrade \$http_upgrade;
-        proxy_set_header Connection "upgrade";
+        proxy_set_header Connection 'Upgrade';
         proxy_set_header Host \$host;
         proxy_set_header X-Real-IP \$remote_addr;
         proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
         proxy_set_header X-Forwarded-Proto \$scheme;
+        proxy_buffering off;
+        proxy_read_timeout 86400s;
+        proxy_send_timeout 86400s;
+        proxy_connect_timeout 86400s;
+        proxy_redirect off;
+        proxy_cache_bypass \$http_upgrade;
     }
 }
 EOL
